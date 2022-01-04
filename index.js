f15d9d2e556bcbe7acdb78a9e46bbc4502bb3c8d
Szoták Simon <szotak.simon16@gmail.com>
2022-01-04 16:03:37 +0100

    checkboxok

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+let todoLista = []
+
+function megjelenit() {
+    let lista = document.getElementById("lista")
+    lista.innerHTML = ""
+    todoLista.forEach(function(element){
+        let li = document.createElement("li")
+        li.innerHTML = element.text
+        let input = document.createElement("input")
+        input.type = "checkbox"
+        input.checked = element.done == true || element.done == '1'
+
+        input.addEventListener('input', function(event){
+            let todoItem = {
+                id: element.id,
+                text: element.text,
+                done: !(element.done == true || element.done == '1'),
+            }
+            fetch('http://localhost:8080/todos/' + todoItem.id, {
+                method: 'PUT',
+                body: JSON.stringify(todoItem),
+            }).then(function(response){
+                if (response.ok){
+                    element.done = todoItem.done
+                }
+                megjelenit()
+            }).catch(function(error){
+                window.alert("Hiba")
+                megjelenit()
+            })
+
+        })
+
+        li.prepend(input)
+        lista.appendChild(li)
+    })
+}
+
+
 window.addEventListener("DOMContentLoaded", function() {
         fetch('http://localhost:8080/todos')
             .then(function(response) {
@@ -7,15 +46,11 @@ window.addEventListener("DOMContentLoaded", function() {
                     return response.json()
                 }
             }).then(function(eredmeny) {
+                todoLista = eredmeny
                 console.log(eredmeny)
-                let lista = document.getElementById("lista")
-                eredmeny.forEach(function(element){
-                    let li = document.createElement("li")
-                    li.innerHTML = element.text
-                    lista.appendChild(li)
-                })
+                megjelenit()
             }).catch(function(error) {
-                //window.alert("HIBA!!!" + error.message)
+                window.alert("HIBA!!!" + error.message)
             })
 
         document.getElementById("todoButton")
@@ -30,6 +65,8 @@ window.addEventListener("DOMContentLoaded", function() {
                     body: JSON.stringify(todoItem),
                 }).then(function(response){
                     if (response.ok){
+                        todoLista.push(todoItem)
+                        megjelenit()
                         window.alert("Siker")
                     }else{
                         throw new Error(response.statusText)
